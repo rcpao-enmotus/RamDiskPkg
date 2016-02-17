04de55c1cd77802398f08cf76736d370f46b7918
Roger C. Pao <rcpao.enmotus@gmail.com>
2016-02-17 12:09:49 -0800

    *.img now only has HelloWorld.efi as \EFI\BOOT\bootx64.efi

--- RamDisk/xxd-i.sh
+++ RamDisk/xxd-i.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# FAIL: cat EFI-1_44MB.img | xxd -i diskimage > diskimage-1_44MB.h # fails because cat of a binary file drops some bytes.
+# FAIL: cat EFI-1_44MB.img | xxd -i > diskimage-1_44MB.h # fails because cat of a binary file drops some bytes.
 # xxd -i EFI-1_44MB.img > diskimage-1_44MB.h
 # xxd -i EFI-2_88MB.img > diskimage-2_88MB.h
+# xxd -i EFI-16MB.img  > diskimage-16MB.h
 # xxd -i EFI-512MB.img  > diskimage-512MB.h
-xxd -i RamDisk.img > diskimage.h
+xxd -i diskimage.img > diskimage.h
 
 exit
